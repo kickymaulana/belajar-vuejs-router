929420f020086c4eabe690b9ab4281e0fe5002e0
kickymaulana <kickymaulana@gmail.com>
2025-09-29 13:46:50 +0700

    Navigation

--- src/components/User.vue
+++ src/components/User.vue
@@ -2,9 +2,19 @@
 
 <template>
   <h1>User</h1>
-  <div>Put header here</div>
+
+  <ul>
+    <li><RouterLink to="/users/profile">Profile</RouterLink></li>
+    <li><RouterLink to="/users/order">Order</RouterLink></li>
+    <li><RouterLink to="/users/wishlist">Wishlist</RouterLink></li>
+  </ul>
   <RouterView />
   <div>Put footer here</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.router-link-exact-active {
+  color: red;
+  font-weight: bold;
+}
+</style>
