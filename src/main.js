dafbfc997a05d62307de59ebc9d1d28b4ae080c3
kickymaulana <kickymaulana@gmail.com>
2025-09-29 10:53:45 +0700

    perbaikan Route Param

--- src/main.js
+++ src/main.js
@@ -1,26 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from './components/Home.vue';
-import About from './components/About.vue';
-import ProductDetail from './components/ProductDetail.vue';
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+import ProductDetail from "./components/ProductDetail.vue";
 
 const router = createRouter({
   routes: [
     {
-      path: '/',
-      component: Home
+      path: "/",
+      component: Home,
     },
     {
-      path: '/about',
-      component: About
+      path: "/about",
+      component: About,
     },
     {
-      path: '/products/:id',
-      component: ProductDetail
-    }
+      path: "/products/:id",
+      component: ProductDetail,
+    },
   ],
-  history: createWebHistory()
-})
+  history: createWebHistory(),
+});
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
