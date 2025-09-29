4e7288671c2946a2e37817afaaac2782ebb754de
kickymaulana <kickymaulana@gmail.com>
2025-09-29 13:37:34 +0700

    Nested Route

---
+++ src/components/User.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <h1>User</h1>
+  <div>Put header here</div>
+  <RouterView />
+  <div>Put footer here</div>
+</template>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import About from "./components/About.vue";
 import ProductDetail from "./components/ProductDetail.vue";
 import NotFound from "./components/NotFound.vue";
 import ProductSearch from "./components/ProductSearch.vue";
+import User from "./components/User.vue";
+import UserProfile from "./components/UserProfile.vue";
+import UserOrder from "./components/UserOrder.vue";
+import UserWishlist from "./components/UserWishlist.vue";
 
 const router = createRouter({
   routes: [
@@ -25,6 +29,28 @@ const router = createRouter({
     {
       path: "/products/:id(\\d+)?",
       component: ProductDetail,
+    },
+    {
+      path: "/users",
+      component: User,
+      children: [
+        {
+          path: "",
+          component: UserProfile,
+        },
+        {
+          path: "profile",
+          component: UserProfile,
+        },
+        {
+          path: "order",
+          component: UserOrder,
+        },
+        {
+          path: "wishlist",
+          component: UserWishlist,
+        },
+      ],
     },
     {
       path: "/:notfound*",
